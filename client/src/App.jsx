ad470cf525cf7be0761eed7be5f1e5b83939e2da
Aman-Islam-2003 <amanislam019@gmail.com>
2024-06-01 00:05:38 +0500

    Basic file structure

---
+++ client/src/App.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import SignIn from './pages/SignIn'
+import SignOut from './pages/SignOut'
+import About from './pages/About'
+import Header from './components/Header'
+
+const App = () => {
+  return (
+    <BrowserRouter>
+    <Header/>
+    <Routes>
+      <Route path='/' element={<Home/>}/>
+      <Route path='/sign-in' element={<SignIn/>}/>
+      <Route path='/sign-out' element={<SignOut/>}/>
+      <Route path='/about' element={<About/>}/>
+    </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App
