ea6085d95c76ff27961b907679033f49b7575db7
Aman-Islam-2003 <amanislam019@gmail.com>
2024-06-05 00:26:57 +0500

    Update auth.controller.js

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -3,12 +3,23 @@ import bcryptjs from "bcryptjs";
 import { errorHandler } from "../utils/error.js";
 
 export const signup = async (req, res, next) => {
-  const { username, email, password } = req.body;
-  const hashedPassword = bcryptjs.hashSync(password, 10);
-  const newUser = new User({ username, email, password: hashedPassword });
-  try {
-    await newUser.save();
-    res.status(200).json( "User created successfully");
+  console.log(req)
+ // const { username, email, password } = req.body;
+  try{
+    const { username, email, password } = req.body;
+  if (!username || !email || !password)
+    return next(errorHandler(400, "Required Fields cannot be empty"));
+  let user = await User.findOne({
+    email,
+  });
+  if (user) return next(errorHandler(409, "User already Exist"));
+  user = new User({
+    username,
+    email,
+    password,
+  });
+  await user.save();
+  res.status(200).json( "User created successfully");
   } catch (err) {
     next(errorHandler(500, 'error here'))
   }
