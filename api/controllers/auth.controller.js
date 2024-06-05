b990d06418d705266c7ce421de8a9060303e6c63
Aman-Islam-2003 <amanislam019@gmail.com>
2024-06-05 20:23:23 +0500

    Update auth.controller.js

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,9 +1,9 @@
 import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
 import { errorHandler } from "../utils/error.js";
+import jwt from "jsonwebtoken";
 
 export const signup = async (req, res, next) => {
-  console.log(req)
  // const { username, email, password } = req.body;
   try{
     const { username, email, password } = req.body;
@@ -13,10 +13,11 @@ export const signup = async (req, res, next) => {
     email,
   });
   if (user) return next(errorHandler(409, "User already Exist"));
+  const hashedPassword = bcryptjs.hashSync(password,10)
   user = new User({
     username,
     email,
-    password,
+    password:hashedPassword,
   });
   await user.save();
   res.status(200).json( "User created successfully");
@@ -24,3 +25,30 @@ export const signup = async (req, res, next) => {
     next(errorHandler(500, 'error here'))
   }
 };
+
+export const signin = async (req,res,next)=>{
+  try{
+  const {email, password} = req.body;
+    if(!email || !password){
+      return next(errorHandler(400, "Required Fields cannot be empty"));
+    }
+
+    const user = await User.findOne({email});
+    if(!user){
+      return next(errorHandler(409, "User doesn't exist"));
+    }
+    const validPassword = bcryptjs.compareSync(password,user.password);
+    if(!validPassword){
+      return next(errorHandler(409, "Wrong credentials"));
+    }
+    const token = jwt.sign({id:user.id}, process.env.JWT_SECRET)
+    console.log(user)
+    const {password:pass, ...rest} = user._doc;
+    res.cookie("access-token", token, {
+      httpOnly: true,
+    }).status(200).json(rest)
+
+  }catch (err) {
+    next(errorHandler(500, 'error here'))
+  }
+}
