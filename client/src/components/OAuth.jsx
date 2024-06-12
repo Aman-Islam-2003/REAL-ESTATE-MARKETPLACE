31ca4c6ff059332c5487bcf6676c1b2481ee3029
Aman-Islam-2003 <amanislam019@gmail.com>
2024-06-12 23:58:15 +0500

    Update OAuth.jsx

--- client/src/components/OAuth.jsx
+++ client/src/components/OAuth.jsx
@@ -1,17 +1,31 @@
 import React from 'react'
 import { GoogleAuthProvider, getAuth, signInWithPopup } from 'firebase/auth'
 import { app } from '../firebase';
+import { useDispatch } from 'react-redux';
+import { signInSuccess } from '../redux/user/userSlice';
 
 const OAuth = () => {
+    const dispatch = useDispatch();
     const handleGoogleClick = async () => {
         try {
-            console.log("object")
             const provider = new GoogleAuthProvider();
-            console.log(provider, "a")
             const auth = getAuth(app);
-            console.log(auth, "b")
             const result = await signInWithPopup(auth, provider);
-            console.log(result)
+            // console.log(result)
+            const res = await fetch('/api/auth/google', {
+                method:'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    name: result.user.displayName,
+                    email: result.user.email,
+                    photo: result.user.photoURL
+                })
+            })
+            const data = await res.json();
+            dispatch(signInSuccess(data));
+
         } catch (error) {
             console.log("could not sign in with google", error)
         }
