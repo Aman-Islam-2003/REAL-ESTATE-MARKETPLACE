68bb94ef70ce4f107f031c33c4b746c2b2ba6578
Aman-Islam-2003 <amanislam019@gmail.com>
2024-06-16 11:57:54 +0500

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,6 +5,8 @@ import SignIn from './pages/SignIn'
 import About from './pages/About'
 import Header from './components/Header'
 import SignUp from './pages/SignUp'
+import PrivateRoute from './components/PrivateRoute'
+import Profile from './pages/Profile'
 
 const App = () => {
   return (
@@ -15,6 +17,9 @@ const App = () => {
       <Route path='/sign-in' element={<SignIn/>}/>
       <Route path='/sign-up' element={<SignUp/>}/>
       <Route path='/about' element={<About/>}/>
+      <Route  element={<PrivateRoute/>}>
+      <Route path='/profile' element={<Profile/>}/>
+      </Route>
     </Routes>
     </BrowserRouter>
   )
