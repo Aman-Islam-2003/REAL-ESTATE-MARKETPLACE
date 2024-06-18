b7e06f79bd048ab6d845e74e5da046c4db1afa83
Aman-Islam-2003 <amanislam019@gmail.com>
2024-06-18 13:37:15 +0500

    Update index.js

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import cors from "cors";
+import cookieParser from "cookie-parser";
 import userRouter from "./routes/user.route.js";
 import authRouter from "./routes/auth.route.js";
 
@@ -21,6 +22,7 @@ app.use(express.json())
 app.use(cors({
   credentials: true,
 }));
+app.use(cookieParser());
 
 app.listen(4000, () => {
   console.log("Server is running on port 4000");
