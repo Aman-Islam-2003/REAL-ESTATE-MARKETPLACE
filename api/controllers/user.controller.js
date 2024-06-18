85176a6a63588272c7d2123d3129ee8b3942178c
Aman-Islam-2003 <amanislam019@gmail.com>
2024-06-18 13:37:09 +0500

    Update user.controller.js

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,3 +1,39 @@
-export const test = (req,res)=>{
-    res.json("Hello world!!!")
-}
+import bcryptjs from "bcryptjs";
+import { errorHandler } from "../utils/error.js";
+import User from "../models/user.model.js";
+
+export const test = (req, res) => {
+  res.json("Hello world!!!");
+};
+
+export const updateUser = async (req, res, next) => {
+  const { id } = req.params;
+  const { user } = req;
+  if (user.id !== id)
+    return next(errorHandler(401, "You can only update your own account"));
+  try {
+    const { username, email, password, avatar } = req.body;
+    if (password) {
+      password = bcryptjs.hashSync(password, 10);
+    }
+    const updateUser = await User.findByIdAndUpdate(
+      id,
+      {
+        $set: {
+          username,
+          email,
+          password,
+          avatar,
+        },
+      },
+      { new: true }
+    );
+
+    const { password: pass, ...rest } = updateUser._doc;
+    res.status(200).json({
+      rest
+    });
+  } catch (error) {
+    next(error);
+  }
+};
