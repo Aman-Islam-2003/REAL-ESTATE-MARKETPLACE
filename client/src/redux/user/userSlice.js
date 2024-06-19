1f642a63bdff766899156d1f6721468154d110b7
Aman-Islam-2003 <amanislam019@gmail.com>
2024-06-19 12:36:56 +0500

    Update userSlice.js

--- client/src/redux/user/userSlice.js
+++ client/src/redux/user/userSlice.js
@@ -21,9 +21,21 @@ const userSlice = createSlice({
        signInFailure: (state,action)=>{
          state.error = action.payload,
          state.loading = false
-       }
+       },
+        updateUserStart: (state,action)=>{
+         state.loading = true
+       },
+       updateUserSuccess: (state,action)=>{
+        state.loading = false,
+        state.currentUser=action.payload,
+        state.error = null
+      },
+      updateUserFailure: (state,action)=>{
+        state.loading = false,
+        state.error = action.payload
+      }
     }
 });
 
-export const {signInFailure, signInStart, signInSuccess} = userSlice.actions;
+export const {signInFailure, signInStart, signInSuccess, updateUserFailure, updateUserStart, updateUserSuccess} = userSlice.actions;
 export default userSlice.reducer;
