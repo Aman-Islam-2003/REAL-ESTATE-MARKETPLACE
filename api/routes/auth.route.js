b4b980d7fa437ba82724b19daa6d915ccda46c74
Aman-Islam-2003 <amanislam019@gmail.com>
2024-06-20 13:31:01 +0500

    Update auth.route.js

--- api/routes/auth.route.js
+++ api/routes/auth.route.js
@@ -1,10 +1,11 @@
 import {Router} from "express";
-import {google, signin, signup} from '../controllers/auth.controller.js'
+import {google, signin, signup, signOut} from '../controllers/auth.controller.js'
 const router = Router();
 
 //routes
 router.post('/signup', signup);
 router.post('/signin', signin);
 router.post('/google', google);
+router.get('/signout', signOut);
 
 export default router;
