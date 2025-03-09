509e4ab06f58ca1de864c7bbdf10e3a8a94e3126
Aman Islam <amanislam019@gmail.com>
2025-03-09 23:41:52 +0500

    Update auth.controller.js

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,37 +1,37 @@
-// import User from "../models/user.model.js";
-// import bcryptjs from "bcryptjs";
-// import { errorHandler } from "../utils/error.js";
-// import jwt from "jsonwebtoken";
+import User from "../models/user.model.js";
+import bcryptjs from "bcryptjs";
+import { errorHandler } from "../utils/error.js";
+import jwt from "jsonwebtoken";
 
-// export const signup = async (req, res, next) => {
-//   // const { username, email, password } = req.body;
-//   try {
-//     const { username, email, password } = req.body;
-//     if (!username || !email || !password)
-//       return next(errorHandler(400, "Required Fields cannot be empty"));
-//     let user = await User.findOne({
-//       email,
-//     });
-//     if (user) return next(errorHandler(409, "User already Exist"));
-//     const hashedPassword = bcryptjs.hashSync(password, 10);
-//     user = new User({
-//       username,
-//       email,
-//       password: hashedPassword,
-//     });
-//     await user.save();
-//     res.status(200).json("User created successfully");
-//   } catch (err) {
-//     next(errorHandler(500, "error here"));
-//   }
-// };
+export const signup = async (req, res, next) => {
+  // const { username, email, password } = req.body;
+  try {
+    const { username, email, password } = req.body;
+    if (!username || !email || !password)
+      return next(errorHandler(400, "Required Fields cannot be empty"));
+    let user = await User.findOne({
+      email,
+    });
+    if (user) return next(errorHandler(409, "User already Exist"));
+    const hashedPassword = bcryptjs.hashSync(password, 10);
+    user = new User({
+      username,
+      email,
+      password: hashedPassword,
+    });
+    await user.save();
+    res.status(200).json("User created successfully");
+  } catch (err) {
+    next(errorHandler(500, "error here"));
+  }
+};
 
-// export const signin = async (req, res, next) => {
-//   try {
-//     const { email, password } = req.body;
-//     if (!email || !password) {
-//       return next(errorHandler(400, "Required Fields cannot be empty"));
-//     }
+export const signin = async (req, res, next) => {
+  try {
+    const { email, password } = req.body;
+    if (!email || !password) {
+      return next(errorHandler(400, "Required Fields cannot be empty"));
+    }
 
     const user = await User.findOne({ email });
     if (!user) {
